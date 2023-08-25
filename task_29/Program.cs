120565c77c514f55513417814b85a8116b137da0
Natalia Eremina <entalia663@gmail.com>
2023-08-25 11:49:26 +0200

    Task 29 is done

--- task_29/Program.cs
+++ task_29/Program.cs
@@ -4,20 +4,25 @@
 
 Random rnd = new Random();
 
-int[] RandomOfNumber()
+int[] RandomOfNumber(int down, int up)
 {
     int[] nums = new int[8];
     for (int i = 0; i < nums.Length; i++)
     {
-        nums[i] = rnd.Next(0, 100);
+        nums[i] = rnd.Next(down, up);
     }
 
     return nums;
 }
 
+Console.WriteLine("Enter the lower and upper bounds via Enter: ");
+
+int lower = Convert.ToInt32(Console.ReadLine());
+int upper = Convert.ToInt32(Console.ReadLine());
+
 Console.Write($"Here is your massive of number: [");
 
-foreach (int number in RandomOfNumber()) 
+foreach (int number in RandomOfNumber(lower, upper)) 
 {
     Console.Write(" {0}", number);
 } 
