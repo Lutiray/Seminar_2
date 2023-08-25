7a3c9ea0a7582be520ec85b66b401dce32e7c399
Natalia Eremina <entalia663@gmail.com>
2023-08-25 11:15:50 +0200

    Task 29 is done

---
+++ task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Random rnd = new Random();
+
+int[] RandomOfNumber()
+{
+    int[] nums = new int[8];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = rnd.Next(0, 100);
+    }
+
+    return nums;
+}
+
+Console.Write($"Here is your massive of number: [");
+
+foreach (int number in RandomOfNumber()) 
+{
+    Console.Write(" {0}", number);
+} 
+
+Console.Write("]");
